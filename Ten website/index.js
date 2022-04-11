1a0f130cca9149eb22c7b71f2a359e20a5af37ae
gsandoo <rogersddt@naver.com>
2022-04-11 18:18:41 +0900

    modal_creation

--- Ten website/index.js
+++ Ten website/index.js
@@ -1,7 +1,14 @@
-const $modal = document.querySelector("#modal");
-const $open = document.querySelector(".modal-open");
-$open.addEventListener('click',modalOpen);
+//모달 구현
 
-function modalOpen(){
-    $modal.style.display='flex';
+const openButton = document.querySelector(".btn");
+const modal = document.querySelector(".modal");
+const closeButton = modal.querySelector(".close");
+const modalBackground = modal.querySelector(".modal__background");
+
+function displayModal(){
+    modal.classList.toggle("hidden");
 }
+
+openButton.addEventListener("click", displayModal);
+closeButton.addEventListener("click", displayModal)
+modalBackground.addEventListener("click", displayModal);
